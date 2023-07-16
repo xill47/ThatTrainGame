2a527b043013686c5be3c74933376973b91b8d39
xill47 <x-i-ll@hotmail.com>
2023-07-16 11:52:13 +0200

    Add map cursor

--- World/map.gd
+++ World/map.gd
@@ -1,18 +1,13 @@
 extends TileMap
 
-var highlighted_tile: Vector2 = Vector2(0, 0)
+@onready var cursor = %Cursor
 
-@export_group("Highlight")
-@export var highlight_layer_id := 1
-@export var highlight_tileset_source_id := 1
-@export var highlight_atlas_coords := Vector2i(0, 0)
-@export var highlight_alternative_id := 0
+var highlighted_tile: Vector2 = Vector2(0, 0)
 
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		var mouse_pos = get_local_mouse_position()
 		var tile_coords = local_to_map(mouse_pos)
-		erase_cell(highlight_layer_id, highlighted_tile)
 		highlighted_tile = tile_coords
-		set_cell(highlight_layer_id, highlighted_tile, 1, Vector2i(0, 0), 0)
+		cursor.position = map_to_local(tile_coords)
